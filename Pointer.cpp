73beb149801ad772fa19dee8f7792f7bd2d201c3
Gareth Webb <gareth.webb@outlook.com>
2024-07-14 00:48:58 +0100

    Mark stub event handlers as noexcept

--- Pointer.cpp
+++ Pointer.cpp
@@ -24,15 +24,15 @@ Pointer::Pointer(Seat& seat)
 
             self._focus = nullptr;
         },
-        .motion  = [](void *, wl_pointer *, uint32_t, wl_fixed_t, wl_fixed_t){},
-        .button = [](void *, wl_pointer *, uint32_t, uint32_t, uint32_t, uint32_t){},
-        .axis = [](void *, wl_pointer *, uint32_t, uint32_t, wl_fixed_t){},
-        .frame = [](void *, wl_pointer *){},
-        .axis_source = [](void *, wl_pointer *, uint32_t){},
-        .axis_stop = [](void *, wl_pointer *, uint32_t, uint32_t){},
-        .axis_discrete = [](void *, wl_pointer *, uint32_t, int32_t){},
-        .axis_value120 = [](void *, wl_pointer *, uint32_t, int32_t){},
-        .axis_relative_direction = [](void *, wl_pointer *, uint32_t, uint32_t){},
+        .motion  = [](void *, wl_pointer *, uint32_t, wl_fixed_t, wl_fixed_t) noexcept {},
+        .button = [](void *, wl_pointer *, uint32_t, uint32_t, uint32_t, uint32_t) noexcept {},
+        .axis = [](void *, wl_pointer *, uint32_t, uint32_t, wl_fixed_t) noexcept {},
+        .frame = [](void *, wl_pointer *) noexcept {},
+        .axis_source = [](void *, wl_pointer *, uint32_t) noexcept {},
+        .axis_stop = [](void *, wl_pointer *, uint32_t, uint32_t) noexcept {},
+        .axis_discrete = [](void *, wl_pointer *, uint32_t, int32_t) noexcept {},
+        .axis_value120 = [](void *, wl_pointer *, uint32_t, int32_t) noexcept {},
+        .axis_relative_direction = [](void *, wl_pointer *, uint32_t, uint32_t) noexcept {},
     };
 
     _pointer.reset(wl_seat_get_pointer(seat._seat.get()));
