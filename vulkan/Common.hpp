1ec583ecf6690850843bebe6e1a6358c34019d4e
Gareth Webb <gareth.webb@outlook.com>
2024-08-11 15:57:42 +0100

    Minor formatting fix

--- vulkan/Common.hpp
+++ vulkan/Common.hpp
@@ -5,10 +5,10 @@
 #include <exception>
 
 inline constexpr float STAGING_PRIORITY = 0.0f;
-inline constexpr float  LOW_PRIORITY = 0.25f;
-inline constexpr float  NORMAL_PRIORITY = 0.5f;
-inline constexpr float  HIGH_PRIORITY = 0.75f;
-inline constexpr float  RENDER_TARGET_PRIORITY = 1.0f;
+inline constexpr float LOW_PRIORITY = 0.25f;
+inline constexpr float NORMAL_PRIORITY = 0.5f;
+inline constexpr float HIGH_PRIORITY = 0.75f;
+inline constexpr float RENDER_TARGET_PRIORITY = 1.0f;
 
 class BadVkResult final : public std::exception {
 public:
