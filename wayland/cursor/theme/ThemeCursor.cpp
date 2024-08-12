859c2d876f4dd731bc18fa83f0c6b488783ba4ef
Gareth Webb <gareth.webb@outlook.com>
2024-08-12 02:27:36 +0100

    Don't spin up animated cursor thread for single-image cursors

--- wayland/cursor/theme/ThemeCursor.cpp
+++ wayland/cursor/theme/ThemeCursor.cpp
@@ -15,7 +15,11 @@ ThemeCursor::~ThemeCursor() {
 void ThemeCursor::set_pointer(uint32_t serial) {
     _serial = serial;
     _thread_status.clear(std::memory_order_release);
-    _thread = std::thread(&ThemeCursor::thread_entry, this);
+
+    wl_pointer_set_cursor(_pointer, _serial, _surface.get(), _cursor->images[0]->hotspot_x, _cursor->images[0]->hotspot_y);
+    if (_cursor->image_count > 0) {
+        _thread = std::thread(&ThemeCursor::thread_entry, this);
+    }
 }
 
 void ThemeCursor::unset_pointer(uint32_t serial) {
@@ -25,16 +29,12 @@ void ThemeCursor::unset_pointer(uint32_t serial) {
 
 void ThemeCursor::thread_entry() noexcept {
     uint32_t image_index = 0;
-    
-    auto *image = _cursor->images[image_index];
-    wl_pointer_set_cursor(_pointer, _serial, _surface.get(), image->hotspot_x, image->hotspot_y);
-
     while (!_thread_status.test(std::memory_order_relaxed)) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(image->delay));
+        const auto *old_image = _cursor->images[image_index];
+        std::this_thread::sleep_for(std::chrono::milliseconds(old_image->delay));
 
-        const auto *old_image = image;
         image_index = (image_index + 1) % _cursor->image_count;
-        image = _cursor->images[image_index];
+        auto *image = _cursor->images[image_index];
 
         wl_surface_attach(_surface.get(), wl_cursor_image_get_buffer(image), old_image->hotspot_x - image->hotspot_x, old_image->hotspot_y - image->hotspot_y);
         wl_surface_damage_buffer(_surface.get(), 0, 0, image->width, image->height);
