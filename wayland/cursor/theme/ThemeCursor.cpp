1aa1a23e27e1b64519e332397ca1382928c029c8
Gareth Webb <gareth.webb@outlook.com>
2024-08-12 02:25:41 +0100

    Set hotspot correctly if it changes

--- wayland/cursor/theme/ThemeCursor.cpp
+++ wayland/cursor/theme/ThemeCursor.cpp
@@ -25,14 +25,19 @@ void ThemeCursor::unset_pointer(uint32_t serial) {
 
 void ThemeCursor::thread_entry() noexcept {
     uint32_t image_index = 0;
-    while (!_thread_status.test(std::memory_order_relaxed)) {
-        auto *image = _cursor->images[image_index];
-        wl_surface_attach(_surface.get(), wl_cursor_image_get_buffer(image), image->width, image->height);
-        wl_surface_damage_buffer(_surface.get(), 0, 0, image->width, image->height);
-        wl_pointer_set_cursor(_pointer, _serial, _surface.get(), image->hotspot_x, image->hotspot_y);
-        wl_surface_commit(_surface.get());
+    
+    auto *image = _cursor->images[image_index];
+    wl_pointer_set_cursor(_pointer, _serial, _surface.get(), image->hotspot_x, image->hotspot_y);
 
+    while (!_thread_status.test(std::memory_order_relaxed)) {
         std::this_thread::sleep_for(std::chrono::milliseconds(image->delay));
+
+        const auto *old_image = image;
         image_index = (image_index + 1) % _cursor->image_count;
+        image = _cursor->images[image_index];
+
+        wl_surface_attach(_surface.get(), wl_cursor_image_get_buffer(image), old_image->hotspot_x - image->hotspot_x, old_image->hotspot_y - image->hotspot_y);
+        wl_surface_damage_buffer(_surface.get(), 0, 0, image->width, image->height);
+        wl_surface_commit(_surface.get());
     }
 }
